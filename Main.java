63b12222840e339c5c942c6d905d50b2f8be7aa1
OrGanzian <65444074+OrGanzian@users.noreply.github.com>
2020-08-02 18:23:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps ! ! !\ Good Luck !");
+        System.out.println("Hello DevOps ! ! ! \n Good Luck !");
     }
 }
